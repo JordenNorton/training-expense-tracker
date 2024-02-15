fd0acbfa35cbd6d9e9a57049e3b0518928227e60
jordennorton <jordennorton@hotmail.co.uk>
2024-02-15 11:05:17 +0000

    Add test expense

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Welcome to Personal Expense Tracker");
+﻿using ExpenseTracker.Models;
+using ExpenseTracker.Services;
+
+var expenseManager = new ExpenseManager();
+
+Console.WriteLine("Welcome to Personal Expense Tracker");
+
+expenseManager.AddExpense(new Expense {Id = 1, Amount = 200, Category = "Travel", Date = "21/02/2024"});
+
+expenseManager.ListExpenses();
+
+Console.ReadLine();
