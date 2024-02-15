e93b3ab4c38033ddcb1ebad1fbd7cf8d987b39a1
jordennorton <jordennorton@hotmail.co.uk>
2024-02-15 16:02:05 +0000

    Add LoadExpenses method at application start

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -3,9 +3,13 @@ using ExpenseTracker.Services;
 
 var expenseManager = new ExpenseManager();
 
+expenseManager.LoadExpenses(); //load the expense file at application startup
+
 Console.WriteLine("Welcome to Personal Expense Tracker");
 
-expenseManager.AddExpense(new Expense {Id = 1, Amount = 200, Category = "Travel", Date = "21/02/2024"});
+// expenseManager.AddExpense(new Expense {Id = 1, Amount = 200, Category = "Travel", Date = "21/02/2024"});
+// expenseManager.AddExpense(new Expense {Id = 2, Amount = 100, Category = "Food", Date = "12/02/2024"});
+
 
 expenseManager.ListExpenses();
 
