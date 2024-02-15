8e1fdaf72da6daa4ec71005a756099a930e70a41
jordennorton <jordennorton@hotmail.co.uk>
2024-02-15 10:19:43 +0000

    Create basic expense model

---
+++ ExpenseTracker/Models/ExpenseModel.cs
@@ -0,0 +1,9 @@
+namespace ExpenseTracker.Models;
+
+public class ExpenseModel
+{
+    public int Id { get; set; }
+    public int Amount { get; set; }
+    public string Category { get; set; }
+    public string Date { get; set; }
+}
