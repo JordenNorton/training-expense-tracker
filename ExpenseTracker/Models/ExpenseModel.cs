6f2af5c613fc060c0d1819298179745a95eda3e6
jordennorton <jordennorton@hotmail.co.uk>
2024-02-15 10:22:43 +0000

    Suppress nullable warning

--- ExpenseTracker/Models/ExpenseModel.cs
+++ ExpenseTracker/Models/ExpenseModel.cs
@@ -4,6 +4,6 @@ public class ExpenseModel
 {
     public int Id { get; set; }
     public int Amount { get; set; }
-    public string Category { get; set; }
-    public string Date { get; set; }
+    public string? Category { get; set; }
+    public string? Date { get; set; }
 }
