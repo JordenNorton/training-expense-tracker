5f82f64a33efb7e1ab2f78ae8f817d3b40f3bb81
jordennorton <jordennorton@hotmail.co.uk>
2024-02-17 15:25:35 +0000

    Change date type to DateTime
    Signed-off-by: Jorden Norton <jordennorton@hotmail.co.uk>

--- ExpenseTracker/Models/Expense.cs
+++ ExpenseTracker/Models/Expense.cs
@@ -5,5 +5,5 @@ public class Expense
     public int Id { get; set; }
     public int Amount { get; set; }
     public string? Category { get; set; }
-    public string? Date { get; set; }
+    public DateTime Date { get; set; }
 }
